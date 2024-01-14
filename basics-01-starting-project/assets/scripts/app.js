e6652f8f8567f443e4d943941500e6f55967bc83
TEELL <TEELLETT@GMAIL.COM>
2024-01-14 00:52:32 -0600

    add first lesson stuff

---
+++ basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,5 @@
+let currentResult = 0;
+currentResult = currentResult + 5;
+// template literal is the `${variable}`
+let currentResultOperation = `0 + ${currentResult}`
+outputResult(currentResult, currentResultOperation);
